d20f34717d2f814231903db33458ab676a01e0e6
Andrew <drew.rchrds@gmail.com>
2014-03-13 22:24:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scrollsite::Application.config.secret_key_base = '4f49a8b30f8da8614d648bbd1f7abec89ac7496750a48f07c7c235985ce91ee8d73272cfeb42c5f8729e4426325ba51677c465f78ec55f425ae5b288ecba465a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scrollsite::Application.config.session_store :cookie_store, key: '_scrollsite_session'
